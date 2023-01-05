2882a50a1ef85573841256ec70be93f44a53825d
abrarasif11 <faasif929@gmail.com>
2023-01-05 14:34:12 +0600

    login-done

--- src/component/Login/Login.js
+++ src/component/Login/Login.js
@@ -1,11 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import { GoogleAuthProvider } from 'firebase/auth';
+import React, { useContext, useState } from 'react';
+import { toast } from 'react-hot-toast';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../Context/AuthProvider';
 const Login = () => {
+    const [error, setError] = useState('');
+
+  const { signIn, googleSignIn } = useContext(AuthContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || '/';
+  const provider = new GoogleAuthProvider();
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        signIn(email, password)
+          .then(result => {
+            const user = result.user;
+            console.log(user);
+            form.reset();
+            setError('');
+            if (user.emailVerified) {
+              navigate(from, { replace: true });
+            }
+            else {
+              toast.error('Your email is not valid, Please verify email first')
+            }
+          })
+          .catch(error => {
+            console.error(error)
+            setError(error.message);
+          });
+      }
+      const handleGoogleSignIn = () => {
+        googleSignIn(provider)
+            .then((userCredential) => {
+                const user = userCredential.user;
+                console.log(user);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    };
     return (
         <section class="bg-white font-poppins dark:bg-gray-900">
             <div class="container flex items-center justify-center min-h-screen px-6 mx-auto">
-                <form class="w-full max-w-md">
+                <form onSubmit={handleSubmit} class="w-full max-w-md">
                     <h1 class="text-3xl font-semibold text-gray-800 capitalize dark:text-white">Log In To Smooth Styling</h1>
 
                     <div class="relative flex items-center mt-8">
@@ -15,7 +57,7 @@ const Login = () => {
                             </svg>
                         </span>
 
-                        <input type="email" class="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address"/>
+                        <input type="email" id='email' class="block w-full py-3 text-gray-700 bg-white border rounded-lg px-11 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Email address"/>
                     </div>
 
                     <div class="relative flex items-center mt-4">
@@ -25,17 +67,17 @@ const Login = () => {
                             </svg>
                         </span>
 
-                        <input type="password" class="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password"/>
+                        <input type="password" id='password' class="block w-full px-10 py-3 text-gray-700 bg-white border rounded-lg dark:bg-gray-900 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-blue-300 focus:outline-none focus:ring focus:ring-opacity-40" placeholder="Password"/>
                     </div>
 
                     <div class="mt-6">
                         <button class="w-full px-6 py-3 text-sm font-medium tracking-wide  capitalize transition-colors duration-300 transform bg-black rounded-lg hover:bg-gray-900 text-yellow-400 focus:outline-none focus:ring focus:bg-gray-700 focus:ring-opacity-50">
                             Login in
                         </button>
-
-                        <p class="mt-4 text-center text-gray-600 dark:text-gray-400">or Log in with</p>
+                        <p className='text-red-700 mt-7 mb-7'> {error}</p>
+                        <p class="mt-4 text-center text-gray-600 dark:text-gray-400"> or login with Social Media</p>
                            
-                         <button className=' w-full'>  
+                         <button onClick={handleGoogleSignIn} className=' w-full'>  
                         <a  href="#" class="flex items-center justify-center px-6 py-3 mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
                             <svg class="w-6 h-6 mx-2" viewBox="0 0 40 40">
                                 <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.045 27.2142 24.3525 30 20 30C14.4775 30 10 25.5225 10 20C10 14.4775 14.4775 9.99999 20 9.99999C22.5492 9.99999 24.8683 10.9617 26.6342 12.5325L31.3483 7.81833C28.3717 5.04416 24.39 3.33333 20 3.33333C10.7958 3.33333 3.33335 10.7958 3.33335 20C3.33335 29.2042 10.7958 36.6667 20 36.6667C29.2042 36.6667 36.6667 29.2042 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#FFC107" />
@@ -44,7 +86,7 @@ const Login = () => {
                                 <path d="M36.3425 16.7358H35V16.6667H20V23.3333H29.4192C28.7592 25.1975 27.56 26.805 26.0133 27.9758C26.0142 27.975 26.015 27.975 26.0158 27.9742L31.1742 32.3392C30.8092 32.6708 36.6667 28.3333 36.6667 20C36.6667 18.8825 36.5517 17.7917 36.3425 16.7358Z" fill="#1976D2" />
                             </svg>
 
-                            <span class="mx-2">Sign in with Google</span>
+                            <span class="mx-2">Log in with Google</span>
                         </a>
                          </button>
                         <div class="mt-6 text-center ">
