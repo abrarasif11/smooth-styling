a6901bc6b286f3020a7e9960fe57577cdda5f4b1
abrarasif11 <faasif929@gmail.com>
2023-01-26 19:05:04 +0600

    responsive-service-part

--- src/component/Categories/Categories.js
+++ src/component/Categories/Categories.js
@@ -13,7 +13,7 @@ const Categories = () => {
         <div>
               <h3 className='mx-auto text-4xl mt-10 font-poppins
                font-bold'>Our Categories</h3>
-            <div className='mt-10 mb-10 grid mx-10 sm:grid-cols-3 gap-3 ml-10 sm:ml-3'>
+            <div className='grid mt-10 mb-10 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-8 lg:ml-20 sm:ml-10'>
                 {
                     categories.map(category => <Category
                         key={category._id}
