a7cd82ec5c30301ac04bfd28ae0466f191da5e8b
abrarasif11 <faasif929@gmail.com>
2023-01-26 21:36:31 +0600

    polish-login-signup-section

--- src/component/ReSignUp/ReSignUp.js
+++ src/component/ReSignUp/ReSignUp.js
@@ -5,7 +5,7 @@ const ReSignUp = () => {
     return (
         <div className="relative">
             <img
-                src="https://images.pexels.com/photos/3747463/pexels-photo-3747463.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+                src="https://images.unsplash.com/photo-1503951914875-452162b0f3f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 className="absolute inset-0 object-cover w-full h-full"
                 alt=""
             />
@@ -13,7 +13,9 @@ const ReSignUp = () => {
                 <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
                     <div className="flex flex-col items-center justify-between xl:flex-row">
                         <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-                            <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                            <h2 className="max-w-lg mb-6 font-poppins text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+                                <span className='text-yellow-400'> Smooth Styling</span>
+                                <br />
                                 Sign Up! <br className="hidden md:block" />
                                 For New Account {' '}
 
@@ -21,11 +23,12 @@ const ReSignUp = () => {
 
 
                         </div>
-                        <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
+                        <div className="w-full font-poppins max-w-xl xl:px-8 xl:w-5/12">
                             <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
 
                                 <form>
-                                <div className="mb-1  sm:mb-2">
+                                    <h3 className='text-4xl font-semibold mb-5'>Sign Up Here</h3>
+                                    <div className="mb-1  sm:mb-2">
                                         <label
                                             htmlFor="firstName"
                                             className="inline-block mr-[315px] mb-1 font-medium"
@@ -41,7 +44,22 @@ const ReSignUp = () => {
                                             name="firstName"
                                         />
                                     </div>
-
+                                    <div className="mb-1  sm:mb-2">
+                                        <label
+                                            htmlFor="firstName"
+                                            className="inline-block mr-[315px] mb-1 font-medium"
+                                        >
+                                            PhotoURL
+                                        </label>
+                                        <input
+                                            placeholder="Enter your Image Link"
+                                            required
+                                            type="text"
+                                            className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                                            id="firstName"
+                                            name="firstName"
+                                        />
+                                    </div>
                                     <div className="mb-1  sm:mb-2">
                                         <label
                                             htmlFor="firstName"
@@ -78,16 +96,16 @@ const ReSignUp = () => {
                                     <div className="mt-4 mb-2 sm:mb-4">
                                         <button
                                             type="submit"
-                                            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-red-600 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                                            className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-yellow-400 transition duration-200 rounded shadow-md hover:bg-yellow-400 hover:text-black bg-black  focus:shadow-outline focus:outline-none"
                                         >
                                             Sign In
                                         </button>
                                         <br />
-                                      
+
                                     </div>
                                     <div class="mt-6 text-center ">
 
-                                        Already have an account? <Link to='/login' className='hover:underline'> <span className='text-blue-600'> Sign Up</span></Link>
+                                        Already have an account? <Link to='/login' className='hover:underline'> <span className='text-blue-600'> Log In</span></Link>
 
                                     </div>
                                 </form>
--- src/component/Register/ReLogin/ReLogin.js
+++ src/component/Register/ReLogin/ReLogin.js
@@ -5,7 +5,7 @@ const ReLogin = () => {
   return (
     <div className="relative">
       <img
-        src="https://images.pexels.com/photos/3747463/pexels-photo-3747463.jpeg?auto=compress&amp;cs=tinysrgb&amp;dpr=2&amp;h=750&amp;w=1260"
+        src="https://images.unsplash.com/photo-1503951914875-452162b0f3f1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
         className="absolute inset-0 object-cover w-full h-full"
         alt=""
       />
@@ -13,18 +13,23 @@ const ReLogin = () => {
         <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
           <div className="flex flex-col items-center justify-between xl:flex-row">
             <div className="w-full max-w-xl mb-12 xl:mb-0 xl:pr-16 xl:w-7/12">
-              <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
-                Hi There ! <br className="hidden md:block" />
-                Welcome Back{' '}
+              <h2 className="max-w-lg mb-6 font-poppins text-3xl font-bold tracking-tight text-white sm:text-4xl sm:leading-none">
+              Hi There !  
+                <br />
+                 Welcome To 
+                 <br />
+                 <span className='text-yellow-400'>  Smooth Styling{' '}</span>
                 
               </h2>
              
               
             </div>
-            <div className="w-full max-w-xl xl:px-8 xl:w-5/12">
+            <div className="w-full font-poppins max-w-xl xl:px-8 xl:w-5/12">
               <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
 
                 <form>
+                  
+                  <h3 className='text-4xl font-semibold mb-5'>Log In Here</h3>
                   <div className="mb-1  sm:mb-2">
                     <label
                       htmlFor="firstName"
@@ -61,9 +66,9 @@ const ReLogin = () => {
                   <div className="mt-4 mb-2 sm:mb-4">
                     <button
                       type="submit"
-                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-red-600 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                      className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-yellow-400 transition duration-200 rounded shadow-md hover:bg-yellow-400 hover:text-black bg-black focus:shadow-outline focus:outline-none"
                     >
-                      Sign In
+                      Log In
                     </button>
                     <br />
                     <button className=' w-full'>
@@ -81,7 +86,7 @@ const ReLogin = () => {
                   </div>
                   <div class="mt-6 text-center ">
 
-                    Don’t have an account? <Link to='/register' className='hover:underline'> <span className='text-blue-600'> Register</span></Link>
+                    Don’t have an account? <Link to='/register' className='hover:underline'> <span className='text-blue-600'> Sign Up</span></Link>
 
                   </div>
                 </form>
